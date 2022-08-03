e97f92bda2684e94c0334440e5ead582608098f4
Chintsai Hwo <jcglqmoyx@gmail.com>
2022-08-03 16:12:30 +0800

    fix bugs

--- task/scripts/scheduled_tasks.py
+++ task/scripts/scheduled_tasks.py
@@ -1,8 +1,20 @@
+import datetime
 import os
 
 import requests
 
-from task.utils.datetime_util import get_date_str
+
+def get_date_str():
+    yesterday_now = datetime.datetime.now() - datetime.timedelta(days=1)
+    year, month, day = yesterday_now.year, yesterday_now.month, yesterday_now.day
+    res = '%d-' % year
+    if month < 10:
+        res += '0'
+    res += '%d-' % month
+    if day < 10:
+        res += '0'
+    res += '%d' % day
+    return res
 
 
 def get_token():
